3505796160b8e04255279b449d41d4e0904b1669
Vlad Rylov <rylov_vladislav@clout.one>
2023-10-14 21:10:16 +0500

    wip: ci cd

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')()
 
-const db = pgp(`postgres://${process.env.DB_USER}:${process.env.PASSWORD}@localhost:${process.env.PORT_DB}/recipes`)
+const db = pgp(`postgres://${process.env.DB_USER}:${process.env.PASSWORD}@${process.env.DB_HOST}:${process.env.PORT_DB}/recipes`)
 
 module.exports = db
