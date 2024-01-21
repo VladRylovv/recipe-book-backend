3251aeea69d6787b0f50a58bfbfb01b0b5171f95
Vlad Rylov <rylov_vladislav@clout.one>
2024-01-21 19:06:23 +0500

    completed: method remove recipe

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../middlewares/auth.middleware")
 router.get("/getRecipes", recipesController.getRecipes)
 router.get("/getRecipesCheck", recipesController.getRecipesCheck)
 router.post("/checkRecipe/:recipeId", recipesController.checkRecipe)
+router.delete("/removeRecipe/:recipeId", recipesController.removeRecipe)
 router.get("/getDetailRecipe/:recipeId", recipesController.getDetailRecipe)
 router.post("/createRecipe", authMiddleware, recipesController.createRecipe)
 
